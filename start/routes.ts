f865bfc76fc6391f9e394c3bb638b914def2e75a
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-28 02:10:29 +0100

    Updated routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,22 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import PostsController from '#controllers/posts_controller'
+import { middleware } from '#start/kernel'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router.get('up', () => 'Server is up!')
+
+router
+  .group(() => {
+    router
+      .group(() => {
+        router.get('posts', [PostsController, 'index'])
+        router.get('posts/:id', [PostsController, 'show'])
+        router.post('posts', [PostsController, 'store'])
+        router.put('posts/:id', [PostsController, 'update'])
+        router.delete('posts/:id', [PostsController, 'destroy'])
+      })
+      .prefix('/v1')
+  })
+  .prefix('/api')
+  .use(middleware.auth({ guards: ['basicAuth'] }))
