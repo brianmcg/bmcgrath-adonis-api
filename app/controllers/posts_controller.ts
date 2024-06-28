2ad5a208170620923fb61b675da6389287b59b02
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-28 22:30:02 +0100

    Added mailer code

--- app/controllers/posts_controller.ts
+++ app/controllers/posts_controller.ts
@@ -1,69 +1,85 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import mail from '@adonisjs/mail/services/main'
+import env from '#start/env'
 import Post from '#models/post'
 
+const sender = env.get('MAILTRAP_SENDER') || ''
+const recipient = env.get('EMAIL_RECIPIENT') || ''
+
+console.log({ sender, recipient })
+
 export default class PostsController {
-	/**
-	 * Display a list of resource
-	 */
-	async index({ response }: HttpContext) {
-		const posts = await Post.all()
-		return response.status(200).json(posts)
-	}
+  /**
+   * Display a list of resource
+   */
+  async index({ response }: HttpContext) {
+    const posts = await Post.all()
+    return response.status(200).json(posts)
+  }
 
-	/**
-	 * Show individual record
-	 */
-	async show({ params, response }: HttpContext) {
-		const post = await Post.find(params.id)
+  /**
+   * Show individual record
+   */
+  async show({ params, response }: HttpContext) {
+    const post = await Post.find(params.id)
 
-		if (post) {
-			return response.status(200).json(post)
-		}
+    if (post) {
+      return response.status(200).json(post)
+    }
 
-		return response.status(404).json({ message: 'Not found!' })
-	}
+    return response.status(404).json({ message: 'Not found!' })
+  }
 
-	/**
-	 * Handle form submission for the create action
-	 */
-	async store({ request, response }: HttpContext) {
-		const { name, address, message } = request.body()
-		const post = await Post.create({ name, address, message })
-		return response.status(201).json(post)
-	}
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request, response }: HttpContext) {
+    const { name, address, message } = request.body()
+    const post = await Post.create({ name, address, message })
 
-	/**
-	 * Handle form submission for the edit action
-	 */
-	async update({ params, request, response }: HttpContext) {
-		const post = await Post.find(params.id)
+    await mail.send((message) => {
+      message
+        .to(recipient)
+        .from(`"Mailtrap 📧" <${sender}>`)
+        .subject(`Mailtrap message from ${name}`)
+        .htmlView('emails/post_email_html', { name, address, message })
+    })
 
-		if (post) {
-			const { name, address, message } = request.body()
+    return response.status(201).json(post)
+  }
 
-			post.name = name
-			post.address = address
-			post.message = message
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request, response }: HttpContext) {
+    const post = await Post.find(params.id)
 
-			await post.save()
+    if (post) {
+      const { name, address, message } = request.body()
 
-			return response.status(200).json(post)
-		}
+      post.name = name
+      post.address = address
+      post.message = message
 
-		return response.status(404).json({ message: 'Not found!' })
-	}
+      await post.save()
 
-	/**
-	 * Delete record
-	 */
-	async destroy({ params, response }: HttpContext) {
-		const post = await Post.find(params.id)
+      return response.status(200).json(post)
+    }
 
-		if (post) {
-			await post.delete()
-			return response.status(204).json(null)
-		}
+    return response.status(404).json({ message: 'Not found!' })
+  }
 
-		return response.status(404).json({ message: 'Not found!' })
-	}
+  /**
+   * Delete record
+   */
+  async destroy({ params, response }: HttpContext) {
+    const post = await Post.find(params.id)
+
+    if (post) {
+      await post.delete()
+      return response.status(204).json(null)
+    }
+
+    return response.status(404).json({ message: 'Not found!' })
+  }
 }
