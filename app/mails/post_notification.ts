8d8f61325bdba2653f3d864877bb19b216f1e7a8
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-29 15:25:37 +0100

    Fixed email

--- app/mails/post_notification.ts
+++ app/mails/post_notification.ts
@@ -1,14 +1,12 @@
 import { BaseMail } from '@adonisjs/mail'
 import env from '#start/env'
 import Post from '#models/post'
-const sender = env.get('MAILTRAP_SENDER', '')
-const recipient = env.get('EMAIL_RECIPIENT', '')
 import logger from '@adonisjs/core/services/logger'
 
 export default class PostNotification extends BaseMail {
-  to = recipient
-  from = `"Mailtrap 📧" <${sender}>`
   post
+  sender = env.get('MAILTRAP_SENDER', '')
+  recipient = env.get('EMAIL_RECIPIENT', '')
 
   constructor(post: Post) {
     super()
@@ -16,13 +14,16 @@ export default class PostNotification extends BaseMail {
   }
 
   prepare() {
-    logger.info('Sending mail to %s', recipient)
+    logger.info('Sending mail to %s', this.recipient)
 
     const title = `Message from ${this.post.name}`
     const reply = `Reply to ${this.post.address}`
     const paragraphs = this.post.message.split('\n').filter(Boolean)
 
     this.message
+      .to(this.recipient)
+      .replyTo(this.post.address)
+      .from(`"Mailtrap 📧" <${this.sender}>`)
       .subject(`Mailtrap message from ${this.post.name}`)
       .textView('emails/post_email_text', { title, paragraphs, reply })
       .htmlView('emails/post_email_html', { title, paragraphs, reply })
