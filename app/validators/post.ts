7f749e598e9837d09251a4466d76cc1b209d7008
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-29 21:29:19 +0100

    Formatted code

--- app/validators/post.ts
+++ app/validators/post.ts
@@ -4,20 +4,20 @@ import vine from '@vinejs/vine'
  * Validates the post's creation action
  */
 export const createPostValidator = vine.compile(
-	vine.object({
-		name: vine.string().trim().escape(),
-		address: vine.string().trim().escape(),
-		message: vine.string().trim().escape(),
-	})
+  vine.object({
+    name: vine.string().trim().escape(),
+    address: vine.string().trim().escape(),
+    message: vine.string().trim().escape(),
+  })
 )
 
 /**
  * Validates the post's update action
  */
 export const updatePostValidator = vine.compile(
-	vine.object({
-		name: vine.string().trim().escape(),
-		address: vine.string().trim().escape(),
-		message: vine.string().trim().escape(),
-	})
+  vine.object({
+    name: vine.string().trim().escape(),
+    address: vine.string().trim().escape(),
+    message: vine.string().trim().escape(),
+  })
 )
